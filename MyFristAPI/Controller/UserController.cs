8e7454babedf8fc31eec499bc435d800b7a58cff
Jenni <jennifermbecegato@gmail.com>
2025-02-02 20:34:48 -0300

    RocketSeat Modulo1 e Api

---
+++ MyFristAPI/Controller/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFristAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(User), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public IActionResult Get(int id)
+        {
+            var response = new User
+            {
+                Id = id,
+                Age = 7,
+                Name = "Momó"
+            };
+            return Ok(response);
+
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("momo");
+        }
+    }
+}
